c0b232a67ca458ed99ffa850d8736fd0d3d01c09
kyungSong <uranos02@gmail.com>
2020-06-23 21:51:44 +0900

    commit

--- store/index.js
+++ store/index.js
@@ -10,7 +10,7 @@ export const state = () => ({
   
   export const actions = {
     async nuxtServerInit({ commit }) {
-      let files = await require.context('../assets/content/blog/', false, /\.json$/);
+      let files = await require.context('~/assets/content/blog/', false, /\.json$/);
       let blogPosts = files.keys().map(key => {
         let res = files(key);
         res.slug = key.slice(2, -5);
