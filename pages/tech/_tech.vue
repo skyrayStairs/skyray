fa1b483f774ce0aa64ddd79be6ae80677c068dd0
kyungSong <uranos02@gmail.com>
2020-08-14 15:08:46 +0900

    commit 1

--- pages/tech/_tech.vue
+++ pages/tech/_tech.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="columns">
-    <div class="column is-half">
-      <div class="box">
-        <div class="content">
-          <div class="title">{{blogPost.en_title}}</div>
-          <p v-html="$md.render(blogPost.en_body)"></p>
+  <div class="container">
+    <div class="columns is-variable is-1">
+      <div class="column is-half">
+        <div class="box">
+          <div class="content">
+            <div class="title">{{blogPost.en_title}}</div>
+            <p v-html="$md.render(blogPost.en_body)"></p>
+          </div>
         </div>
+        <author-card :auth_name="blogPost.en_author" :auth_image="blogPost.original_author"></author-card>
       </div>
-      <author-card :auth_name="blogPost.en_author" :auth_image="blogPost.original_author"></author-card>
-    </div>
-    <div class="column is-half">
-      <div class="box">
-        <div class="content">
-          <div class="title">{{blogPost.kr_title}}</div>
-          <p v-html="$md.render(blogPost.kr_body)"></p>
+      <div class="column is-half">
+        <div class="box">
+          <div class="content">
+            <div class="title">{{blogPost.kr_title}}</div>
+            <p v-html="$md.render(blogPost.kr_body)"></p>
+          </div>
         </div>
+        <author-card :auth_name="blogPost.en_author" :auth_image="blogPost.original_author"></author-card>
       </div>
     </div>
-    <author-card :auth_name="blogPost.en_author" :auth_image="blogPost.original_author"></author-card>
   </div>
 </template>
 
